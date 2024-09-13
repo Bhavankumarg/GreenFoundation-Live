825d69cdfce5d2a7d104e4ff5058a2c6d3087ab3
Bhavankumarg <gbhavanbkr@gmail.com>
2024-09-13 12:34:01 +0530

    meta title and description added

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -4,29 +4,42 @@ import Footer from '@/components/shared/Footer/Footer';
 import { Col, Container, Row } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NextSeo } from 'next-seo';
+import Head from 'next/head';
 
-const Post = ({ data }) => {
-  const post = data[0]; // Assuming data is an array with a single post
+const Post = ({ data, slug }) => {
+  const post = data[0];
+  const metaTitle = post?.acf.meta_title || post?.title?.rendered;
+  const metaDescription = post?.acf.meta_description || post?.description?.rendered;
 
   return (
     <div>
-      <NextSeo
-        noindex={false}
-        nofollow={false}
-      />
+     
+      <Head>
+        <title>{metaTitle}</title>
+        <meta name="description" content={metaDescription} />
+        <link rel="canonical" href={`https://greenfoundation.in/blog/${slug}`} />
+        <meta name="robots" content="index, follow"/>
+      </Head>
+
       <Header />
       <div className='w-full g-0'>
         <Container fluid className="g-0" style={{ backgroundImage: `url("/aboutus/awards-recog/awards_banner.png")`, height: "70vh", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
           <Row className="g-0 flex-column flex-lg-row bg-opacity-50 p-2 bg-black h-100">
             <Container className='d-flex flex-column justify-content-center h-100'>
-            <Col className="text-center d-flex justify-content-center flex-column lh-sm">
-              <div dangerouslySetInnerHTML={{ __html: post['title']['rendered'] }} className="text-white fs-1" />
-            </Col>
+              <Col className="text-center d-flex justify-content-center flex-column lh-sm">
+                {/* Ensure the post title is consistent between server and client */}
+                {post?.title?.rendered && (
+                  <div dangerouslySetInnerHTML={{ __html: post['title']['rendered'] }} className="text-white fs-1" />
+                )}
+              </Col>
             </Container>
           </Row>
         </Container>
         <Container className="pt-5">
-          <div dangerouslySetInnerHTML={{ __html: post['content']['rendered'] }} />
+          {/* Ensure the post content is consistent between server and client */}
+          {post?.content?.rendered && (
+            <div dangerouslySetInnerHTML={{ __html: post['content']['rendered'] }} />
+          )}
         </Container>
       </div>
       <Footer />
@@ -42,7 +55,7 @@ export async function getServerSideProps(context) {
   const data = await res.json();
 
   return {
-    props: { data },
+    props: { data, slug },  // Pass slug to props
     notFound: !data || data.length === 0,
   };
 }
